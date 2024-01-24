f202d0192c588e14acb64988c2fe5ddda5eff13b
Raj-dhakar <rajdhakar062003@gmail.com>
2024-01-24 09:00:37 +0530

    First commit

---
+++ app/src/main/java/com/example/myshoppinglistapp/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.myshoppinglistapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRowScopeInstance.align
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.myshoppinglistapp.ui.theme.MyShoppingListAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyShoppingListAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    shoppingListApp()
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun shoppingListApp(){
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Button(onClick = {}) {
+                Text(
+                    text = "Add items",
+                    modifier = Modifier.align(Alignment.CenterVertically),
+                    style = androidx.compose.ui.text.TextStyle(fontSize = 18.sp)
+                ) }
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun shoppingListAppPreview(){
+        shoppingListApp()
+    }
+}
+

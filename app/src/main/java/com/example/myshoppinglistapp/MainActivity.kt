6f00e05e144974c7169914cb687b789b2c4e9267
Raj-dhakar <rajdhakar062003@gmail.com>
2024-01-24 09:13:39 +0530

    Second commit

--- app/src/main/java/com/example/myshoppinglistapp/MainActivity.kt
+++ app/src/main/java/com/example/myshoppinglistapp/MainActivity.kt
@@ -3,19 +3,12 @@ package com.example.myshoppinglistapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.FlowRowScopeInstance.align
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import com.example.myshoppinglistapp.ui.theme.MyShoppingListAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,22 +24,6 @@ class MainActivity : ComponentActivity() {
                     shoppingListApp()
                 }
             }
-        }
-    }
-
-    @Composable
-    fun shoppingListApp(){
-
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-        ) {
-            Button(onClick = {}) {
-                Text(
-                    text = "Add items",
-                    modifier = Modifier.align(Alignment.CenterVertically),
-                    style = androidx.compose.ui.text.TextStyle(fontSize = 18.sp)
-                ) }
         }
     }
 
--- app/src/main/java/com/example/myshoppinglistapp/shoppingList.kt
+++ app/src/main/java/com/example/myshoppinglistapp/shoppingList.kt
@@ -1,2 +1,23 @@
 package com.example.myshoppinglistapp
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
+@Composable
+fun shoppingListApp(){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = { /*TODO*/ },
+            modifier = Modifier.align(Alignment.CenterHorizontally)) {
+
+            Text(text = "Add items")
+        }
+    }
+}
